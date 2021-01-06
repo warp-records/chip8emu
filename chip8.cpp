b865f11677a862159465484d69ac744355774567
Rift8844 <32559038+theultraman20@users.noreply.github.com>
2021-01-06 03:32:45 +0000

    Update chip8.cpp

--- chip8.cpp
+++ chip8.cpp
@@ -1,6 +1,8 @@
 #include "chip8.h"
-#include <vector>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
 
 
 void Chip8::initialize() {
@@ -14,7 +16,23 @@ void Chip8::initialize() {
   	//load fontset
   	for(int i = 0; i < 80; ++i)
     	memory[i] = chip8_fontset[i];
+
+ 	loadGame("game.bin");
 };
+
+void Chip8::loadGame(std::string const& name) {
+	int constexpr maxLen = 3584;
+
+	std::ifstream is(name, std::ios::binary);
+
+	if (!is || is.tellg() > maxLen)
+   		//man fuck you
+    	throw std::system_error();
+
+    /*first 512 bytes are typically reserved 
+    for the interpreter*/
+    is.read(memory[512], is.tellg());
+}
 
 
 
@@ -23,16 +41,208 @@ void Chip8::emulateCycle() {
 	//merge two bytes of the pc
 	opcode = memory[pc] << 8 | memory[pc + 1];
 
-	//opcode nibbles
+	//opcode nibbles, MUCH more readable, and efficent
 	unsigned char nibble[4];
 	//fancy code to get each nibble (4 bits) from the opcode
 	for (int i = 3, j = 0xF000; i >= 0; i--, j >>= 4) {
 		nibble[i] = (opcode & j) >> i*4;
 	}
 
+	/*Warning: slightly cringey code.
+	These are notations that make it more
+	convenient to read from and write to
+	registers that are frequently accessed.
+	These registers represent the "X" 
+	(second nibble) and "Y" (third nibble)
+	in the code.*/
+	unsigned char& regX = reg[nibble[1]];
+	unsigned char& regY = reg[nibble[2]];
+	//the "F" register
+	unsigned char& regF = reg[0xF];
+	unsigned short nnn 	= opcode & 0x0FFF;
+	unsigned short nn 	= opcode & 0x00FF;
+	unsigned short n 	= opcode & 0x000F;
 
 	switch (nibble[0]) {
-		
+
+		case 0x0:
+			/*(apparently) depracated, and obscure opcode... 
+			maybe don't implement?*/
+			if (opcode != 0x00E0 && opcode != 0x00EE)
+				opcode = memory[nnn];
+
+			switch (opcode) {
+				case 0x00E0:
+					//clearDisplay();
+					for (int x = 0; x < 64; x++) {
+						for (int y = 0; y < 32; y++) {
+							gfx[x][y] = 0;
+						}
+					}
+					break;
+
+				case 0x00EE:
+					pc = stack[sp] - 2;
+					sp--;
+					break;
+			}
+
+		//1NNN: jump to NNN
+		case 0x1:
+			//subtract two since it will be added at the end
+			pc = nnn - 2;
+			break;
+
+		//2NNN: function call at nnn
+		case 0x2:
+			stack[sp] = pc;
+			sp++;
+			pc = nnn - 2;
+			break;
+
+
+		/*OUT OF BOUNDS POSSIBLE on 3XNN and 4XNN,
+		make sure to change later!*/
+		//3XNN: if V[X] == NN, skip instruction
+		case 0x3:
+			if (regX == nn) {
+				pc += 2;
+			}
+			break;
+
+		//4XNN: if V[X] != NN, skip instruction
+		case 0x4:
+			if (regX != nn) {
+				pc += 2;
+			}
+			break;
+
+		//5XY0: if V[X] == V[Y], skip instruction
+		case 0x5:
+			if (regX == regY) {
+				pc += 2;
+			}
+			break;
+
+		//6XNN: set V[X] to NN
+		case 0x6:
+			regX = nn;
+			break;
+
+		//7XNN: increment V[X] by NN
+		case 0x7:
+			regX += nn;
+			break;
+
+		case 0x8:
+			switch (n) {
+				//8XY0: set V[X] to V[Y]
+				case 0x0:
+					regX = regY;
+					break;
+
+				case 0x1:
+					regX |= regY;
+					break;
+
+				case 0x2:
+					regX &= regY;
+					break;
+
+				case 0x3:
+					regX ^= regY;
+					break;
+
+				//INCOMPLETE, change VF
+				case 0x4:
+					regF = (regX - regY) > 255;
+					regX += regY;
+					break;
+
+				//INCOMPLETE, change VF
+				case 0x5:
+					regF = (regX - regY) >= 0;
+					regX -= regY;
+					break;
+
+				//INCOMPLETE
+				case 0x6:
+					regF = regX >> 7;
+					regX >>= 1;
+					break;
+
+				//INCOMPLETE
+				case 0x7:
+					regF = (regY - regX) < 0;
+					regX = regY - regX;
+					break;
+
+				//INCOMPLETE
+				case 0xE:
+					regF = (regX << 7) >> 7;
+					regX <<= 1;
+					break;
+			}
+			break;
+
+		case 0x9:
+			if (regX != regY) {
+				pc += 2;
+			}
+			break;
+
+		case 0xA:
+			idx = nnn;
+			break;
+
+		case 0xB:
+			pc = reg[0] + nnn - 2;
+			break;
+
+		case 0xC:
+			regX = rand() & nn;
+			break;
+
+		/*Display opcode. This is the most 
+		complex opcode, come back to it later.*/
+		/*case 0xD: {
+			int width = 8;
+			int height = n + 1;
+
+			for (int x = regX; x < regX + width; x++) {
+				for (int y = regY; y < regY + height; y++) {
+
+				}
+			}
+		}*/
+		case 0xF:
+			switch (nn) {
+				case 0x07:
+					regX = delayTimer;
+					break;
+
+				case 0x15:
+					delayTimer = regX;
+					break;
+
+				case 0x1E:
+					idx += regX;
+					break;
+
+				case 0x55:
+					for (int i = 0; i <= nibble[1]; i++) {
+						memory[i+idx] = reg[i];
+					}
+					break;
+
+				case 0x65:
+					for (int i = 0; i <= nibble[1]; i++) {
+						reg[i] = memory[i+idx];
+					}
+					break;
+			}
+			break;
+
 	}
 
 	pc += 2;
@@ -41,3 +251,15 @@ void Chip8::emulateCycle() {
 /*void Chip8::execOpcode(opcode) {
 
 }*/
+
+
+/*
+USE THIS
+
+//opcode nibbles
+unsigned char nibble[4];
+//fancy code to get each nibble (4 bits) from the opcode
+for (int i = 3, j = 0xF000; i >= 0; i--, j >>= 4) {
+	nibble[i] = (opcode & j) >> i*4;
+}
+*/
