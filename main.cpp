d5022879448cb8f8af3e0ba777b6ae54047b794e
Rift8844 <32559038+theultraman20@users.noreply.github.com>
2021-01-21 02:39:05 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,20 @@
 #include <iostream>
-//#include "chip8.h"
+#include "chip8.h"
 
-/*
-Huge thanks to Laurence Miller's
-tutorial on multigesture.net for 
-making this possible!
+//Laurence Miller's Chip8 Emuo tutorial SUCKS!
 
-http://www.multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter/
+int main() {
+	Chip8 emulator;
+	emulator.initialize("test_opcode.ch8");
 
-Or, alternatively
-https://web.archive.org/web/20201111234103/http://www.multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter/
-*/
+	for (int i = 0; true; i++) {
+		//std::cout << std::dec << "Cycle Count: " << i << '\n';
+		emulator.executeCycle();
 
-/* These dependend on the fact that 
-a short is 2 bytes, so if this is
-different on your machine, this 
-MAY BREAK the emulator! */
-
-
-int main(int argc, char** argv) {
-	unsigned short opcode = 0xABCD;
-	
-	unsigned char nibble[4];
-
-	for (int i = 3, j = 0xF000; i >= 0; i--, j >>= 4) {
-		nibble[i] = (opcode & j) >> i*4;
+		if (!emulator.debugFlag)
+			emulator.gfxDraw(std::cout);
+		//std::cin.get();
 	}
+
+	return 0;
 }
