4318217c2ccf95c8161c9277f31aeae3a72c35a3
Rift8844 <32559038+theultraman20@users.noreply.github.com>
2021-01-21 02:39:18 -0500

    Update chip8.h

--- chip8.h
+++ chip8.h
@@ -1,14 +1,32 @@
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <map>
 #include <cmath>
+#include <sstream>
+/*sleep libraries, but they're
+different for Windows and Linux
+*/
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
 
 
 class Chip8 {
+
+	static constexpr int screenWidth = 64;
+	static constexpr int screenHeight = 32;
+
+	//in Hz, NOT MHz
+	//appears to be broken lol
+	static constexpr int clockSpeed = 50;
+
 	//general registers
-	unsigned char reg[16];
+	unsigned char reg[16] { 0 };
 	//program counter
-	unsigned short pc;
+	unsigned int pc;
 	/*index register
 	changed it to "idx" instead of 
 	"I" since "I" is very ugly and
@@ -18,14 +36,12 @@ class Chip8 {
 	//currently loaded opcode
 	unsigned short opcode;
 	//vm memory
-	unsigned char memory[4096];
-	//stack memory
-	unsigned short stack[16];
+	unsigned char memory[4096] {0};
 	//stack pointer
-	unsigned short sp;
+	unsigned short* sp;
 
 	//graphics buffer
-	unsigned char gfx[64][32];
+	unsigned char gfx[screenWidth][screenHeight] { 0 };
 
 	//self explanatory lol
 	unsigned char delayTimer;
@@ -70,6 +86,10 @@ class Chip8 {
 	  0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 	};
 
+	/*number of cycles since the last time
+	the timers were decremented*/
+	unsigned int cycleNumber = 0;
+
 	/*map each character to its memory address
 	 in the font set, for instruction FX29*/
 	std::map<char, unsigned short> fontsetMap {};
@@ -91,7 +111,21 @@ class Chip8 {
 	void loadGame(std::string const& fileName);
 
 public:
-	void initialize();
+	bool debugFlag = false;
 
-	void emulateCycle();
+	void initialize(std::string const& game);
+
+	void executeCycle();
+
+	auto getGraphicsBuffer() const {
+		return gfx;
+	}
+
+	std::pair<int, int> screenDimensions() {
+		return std::make_pair(screenWidth, screenHeight);
+	}
+
+	std::ostream& memdump(std::ostream& os);
+
+	std::ostream& gfxDraw(std::ostream& os);
 };
