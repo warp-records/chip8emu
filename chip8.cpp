f6e3e07427a48a54fe42582583ce506f901a0eee
Rift8844 <32559038+theultraman20@users.noreply.github.com>
2021-01-21 02:39:33 -0500

    Update chip8.cpp

--- chip8.cpp
+++ chip8.cpp
@@ -1,13 +1,20 @@
 #include "chip8.h"
+#include <bitset>
 
 
-void Chip8::initialize() {
+
+void Chip8::initialize(std::string const& game) {
+
 	//Program counter starts at byte 512, or address 0x200
 	pc = 512;
 	//clear these
   	opcode = 0;
   	idx = 0;
-  	sp = 0;
+  	//set the stack pointer to the beginning of memory
+  	sp = (unsigned short*) memory;
+
+  	soundTimer = 0;
+  	delayTimer = 0;
 
   	/*load fontset, with 16 different
   	characters, and 5 bytes per character*/
@@ -24,35 +31,44 @@ void Chip8::initialize() {
     	}
     }
 
- 	loadGame("game.bin");
+ 	loadGame(game);
 };
 
 void Chip8::loadGame(std::string const& name) {
 	int constexpr maxLen = 3584;
 
-	std::ifstream is(name, std::ios::binary);
+	std::ifstream is(name, std::ios::binary | std::ios::in);
+	
+	is.seekg(0, is.end);
+	int fileSize = is.tellg();
+	is.seekg(0, is.beg);
 
-	if (!is || is.tellg() > maxLen)
-   		//man fuck you
+	if (!is || fileSize > maxLen) {
+		std::cout << "Error: file size is " << fileSize << '\n';
+   		/*man fuck you whoever you
+   		are lmao*/
     	throw std::system_error();
+    }
 
     /*first 512 bytes are typically reserved 
     for the interpreter*/
-    is.read(memory[512], is.tellg());
+    is.read(reinterpret_cast<char*> (memory+512), fileSize);
 }
 
 
 
+void Chip8::executeCycle() {
 
-void Chip8::emulateCycle() {
+	if (pc > 4095)
+		throw std::exception();
 	//merge two bytes of the pc
-	opcode = memory[pc] << 8 | memory[pc + 1];
+	opcode = (memory[pc] << 8) | (memory[pc + 1]);
 
 	//opcode nibbles, MUCH more readable, and efficent
 	unsigned char nibble[4];
 	//fancy code to get each nibble (4 bits) from the opcode
 	for (int i = 3, j = 0xF000; i >= 0; i--, j >>= 4) {
-		nibble[i] = (opcode & j) >> i*4;
+		nibble[3-i] = (opcode & j) >> i*4;
 	}
 
 	/*Warning: slightly cringey code.
@@ -74,26 +90,28 @@ void Chip8::emulateCycle() {
 	switch (nibble[0]) {
 
 		case 0x0:
+			//break;
 			/*(apparently) depracated, and obscure opcode... 
-			maybe don't implement?*/
+			maybe don't implement?
 			if (opcode != 0x00E0 && opcode != 0x00EE)
-				opcode = memory[nnn];
+				opcode = memory[nnn];*/
 
 			switch (opcode) {
 				case 0x00E0:
 					//clearDisplay();
-					for (int x = 0; x < 64; x++) {
-						for (int y = 0; y < 32; y++) {
+					for (int x = 0; x < screenWidth; x++) {
+						for (int y = 0; y < screenHeight; y++) {
 							gfx[x][y] = 0;
 						}
 					}
 					break;
 
 				case 0x00EE:
-					pc = stack[sp] - 2;
-					sp--;
+					pc = *sp;
+					sp -= 2;
 					break;
 			}
+			break;
 
 		//1NNN: jump to NNN
 		case 0x1:
@@ -103,8 +121,8 @@ void Chip8::emulateCycle() {
 
 		//2NNN: function call at nnn
 		case 0x2:
-			stack[sp] = pc;
-			sp++;
+			sp += 2;
+			*sp = pc;
 			pc = nnn - 2;
 			break;
 
@@ -162,31 +180,26 @@ void Chip8::emulateCycle() {
 					regX ^= regY;
 					break;
 
-				//INCOMPLETE, change VF
 				case 0x4:
-					regF = (regX - regY) > 255;
+					regF = (regX - regY) > 0xFF;
 					regX += regY;
 					break;
 
-				//INCOMPLETE, change VF
 				case 0x5:
 					regF = (regX - regY) >= 0;
 					regX -= regY;
 					break;
 
-				//INCOMPLETE
 				case 0x6:
 					regF = regX >> 7;
 					regX >>= 1;
 					break;
 
-				//INCOMPLETE
 				case 0x7:
 					regF = (regY - regX) < 0;
 					regX = regY - regX;
 					break;
 
-				//INCOMPLETE
 				case 0xE:
 					regF = (regX << 7) >> 7;
 					regX <<= 1;
@@ -217,22 +230,21 @@ void Chip8::emulateCycle() {
 		case 0xD: {
 			/*consider making these local to
 			the class itself*/
-			int constexpr screenWidth = 64;
-			int constexpr screenHeight = 32;
 
 			int constexpr width = 8;
-			int height = n + 1;
+			//not n+1 for some reason...
+			int height = n;
 
 			/*wrap around starting positions
 			but DONT wrap around the actual drawings*/
-			int startX = nibble[1] % screenWidth;
-			int startY = nibble[2] % screenHeight;
+			int startX = regX % screenWidth;
+			int startY = regY % screenHeight;
 
 			unsigned char spriteRow;
 
 			//false by default
 			regF = false;
-			
+
 			//clipping.
 			for (int y = 0; y < height && y + startY < screenHeight; y++) {
 				spriteRow = memory[idx + y];
@@ -242,14 +254,20 @@ void Chip8::emulateCycle() {
 
 					/*if the current pixel is XOR'd to zero,
 					set the V[F] flag to true*/
-					if (spriteRow&0x1 && pixel)
+					if (spriteRow&0x80 && pixel)
 						regF = true;
 					/*xor the current pixel with the current
 					bit of the sprite row byte*/
-					pixel ^= (spriteRow&0x1);
-					spriteRow >>= 1;
+					pixel ^= (spriteRow >> 7);
+
+					spriteRow <<= 1;
+
+					if (x < 0 || x > 64 || y < 0 || y > 32)
+						throw std::exception();
 				}
 			}
+
+			break;
 
 		}
 
@@ -290,13 +308,16 @@ void Chip8::emulateCycle() {
 					break;
 
 				case 0x33: {
-					unsigned char val = regX;
-					for (int i = 0; i < 3; i--) {
+					unsigned int val = regX;
+					//std::cout << "BCD Instruction\nVal: " << val;
+
+					for (int i = 0; i < 3; i++) {
 						memory[idx+i] = val%10;
 						val /= 10;
 					}
 					break;
 				}
+				break;
 
 				case 0x55:
 					for (int i = 0; i <= nibble[1]; i++) {
@@ -312,23 +333,85 @@ void Chip8::emulateCycle() {
 			}
 			break;
 
+		default: {
+			std::cout << "Encountered unknown opcode! Terminating!\n";
+			throw std::exception();
+		}
 	}
 
 	pc += 2;
+
+	//count down timers at 60hz
+	if (cycleNumber/clockSpeed*60 >= 1) {
+		delayTimer--;
+		soundTimer--;
+		//clear console, apparently
+
+		//sleep for a certain amount of time
+		usleep(16667);
+
+		cycleNumber = 1;
+	} else {
+		cycleNumber++;
+	}
+
+	//----------------DEBUG-------------
+	if (debugFlag) {
+		std::cout << "Executing opcode 0x" << std::hex << 
+		std::setfill('0') << std::setw(4) << opcode << '\n';
+
+		std::cout <<  "PC: " << pc << '\n';
+		for (int i = 0; i < 16; i++) {
+			std::cout << "Register " << i << ": " << (int) reg[i] << '\n';
+		}
+		std::cout << "Reg Idx: " << (int) idx << '\n';
+		std::cout << "nnn: " << nnn << '\n';
+		std::cout << "nn: " << nn << '\n';
+		std::cout << "n: " << n << '\n';
+		std::cout << "Stack Pointer offset: " << ((long) sp - (long) memory) << '\n';
+	}
+
 }
 
-/*void Chip8::execOpcode(opcode) {
 
+std::ostream& Chip8::memdump(std::ostream& os) {
+	os << "Memory dump:\n";
+	for (int i = 0; i < 4096; i++) {
+	    if (memory[i] != 0) {
+		   	os << "[" << i << 
+		    "]: " << std::hex << (int) memory[i] <<
+		    std::dec << '\n';
+	    }
+	}
+
+	return os;
+}
+
+std::ostream& Chip8::gfxDraw(std::ostream& os) {
+	std::stringstream buffer;
+	/*for (int i = 0; i < 64; i++)
+		os << std::setw(2) << i;*/
+
+	buffer << "\033[H\033[J";
+
+	for (int i = 0; i < 32; i++) {
+		for (int j = 0; j < 64; j++) {
+			buffer << ((int) gfx[j][i] ? "\u2588\u2588" : "  ");
+		}
+		buffer << '\n';
+	}
+
+	return os << buffer.str();
+}
+
+//there was an attempt at least...
+/*std::ostream& Chip8::debugInfo(std::ostream& os, DebugFlags flags) {
+	os << "Debug Info:\n";
+	if (flags & 0x1) {
+		os << ""
+	}
 }*/
 
-
-/*
-USE THIS
-
-//opcode nibbles
-unsigned char nibble[4];
-//fancy code to get each nibble (4 bits) from the opcode
-for (int i = 3, j = 0xF000; i >= 0; i--, j >>= 4) {
-	nibble[i] = (opcode & j) >> i*4;
-}
-*/
+/*Chip8::Chip8() {
+	initialize();
+}*/
