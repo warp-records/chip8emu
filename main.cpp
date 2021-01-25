5fe565391ba19d32997a0b65c26fc8b93d04d003
Rift8844 <32559038+theultraman20@users.noreply.github.com>
2021-01-24 22:15:10 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,60 @@
-#include <iostream>
 #include "chip8.h"
+#include <iostream>
+#include <SDL2/SDL.h>
 
-//Laurence Miller's Chip8 Emuo tutorial SUCKS!
+/* These dependend on the fact that 
+a short is 2 bytes, so if this is
+different on your machine, this 
+MAY BREAK the emulator! */
 
-int main() {
+void render() {
+
+}
+
+
+int main(int argc, char* argv[]) {
+	
+	//SDL shit going on here
+	//window to render to
+	SDL_Window* window = nullptr;
+
+	//the surface contained by the window
+	SDL_Surface* surface = nullptr;
+
+
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+		throw std::exception();
+
+	SDL_Window* win = SDL_CreateWindow("Dael Chip8 Interpreter", 200, 200, 1280, 640, SDL_WINDOW_SHOWN);
+	if (win == nullptr)
+		throw std::exception();
+
+	SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (ren == nullptr)
+		throw std::exception();
+
+	std::cin.get();
+
+	SDL_Quit();
+
+
+	return 0;
+}
+/*
+void oldGameLoop(int argc, char* argv[]) {
+	if (argc != 2)
+		throw std::exception();
+
+
 	Chip8 emulator;
-	emulator.initialize("test_opcode.ch8");
+	emulator.initialize(argv[1]);
 
 	for (int i = 0; true; i++) {
 		//std::cout << std::dec << "Cycle Count: " << i << '\n';
 		emulator.executeCycle();
 
-		if (!emulator.debugFlag)
+		if (!emulator.debugFlag && emulator.drawFlag)
 			emulator.gfxDraw(std::cout);
 		//std::cin.get();
 	}
-
-	return 0;
-}
+}*/
