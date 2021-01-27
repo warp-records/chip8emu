740c2aaf4ef5634f9b51a5041be4e50a7e532c74
Rift8844 <32559038+theultraman20@users.noreply.github.com>
2021-01-27 00:03:14 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,20 +7,34 @@ a short is 2 bytes, so if this is
 different on your machine, this 
 MAY BREAK the emulator! */
 
-void render() {
+void drawGraphics(SDL_Renderer* renderer, SDL_Texture* texture, unsigned int (&renderBuff)[2048], unsigned char const* gfxBuff) {
+	for (int i = 0; i < 32; i++) {
+		for (int j = 0; j < 64; j++) {
+			/*Wait, WHAT? WHY? How? You should
+			really figure out the answers to 
+			these questions later*/
+			renderBuff[j + i*64] = ((gfxBuff[j*32 + i] * 0xFFFFFF00) | 0x000000FF);
+		}
+	}
+
+	SDL_UpdateTexture(texture, NULL, renderBuff, 64 * sizeof(uint32_t));
+
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, texture, NULL, NULL);
+	SDL_RenderPresent(renderer);
 
 }
 
 
+
 int main(int argc, char* argv[]) {
 	
-	//SDL shit going on here
+	//SDL Initialization stuff
 	//window to render to
 	SDL_Window* window = nullptr;
 
 	//the surface contained by the window
 	SDL_Surface* surface = nullptr;
-
 
 	if (SDL_Init(SDL_INIT_VIDEO) != 0)
 		throw std::exception();
@@ -29,17 +43,59 @@ int main(int argc, char* argv[]) {
 	if (win == nullptr)
 		throw std::exception();
 
-	SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);//SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	//SDL_RenderSetLogicalSize(ren, 1280, 640);
 	if (ren == nullptr)
 		throw std::exception();
 
-	std::cin.get();
+	SDL_Texture* texture = SDL_CreateTexture(ren, 
+											SDL_PIXELFORMAT_RGBA8888, 
+											SDL_TEXTUREACCESS_STATIC,
+											64, 32);
+	
+	unsigned int renderBuff[2048];
+
+
+	//Load the game and initailize the emu
+	if (argc != 2)
+		throw std::exception();
+
+	Chip8 emulator;
+	emulator.initialize(argv[1]);
+
+
+	SDL_Event event;
+	//game loop
+	for (int i = 0; true; i++) {
+		//std::cout << std::dec << "Cycle Count: " << i << '\n';
+		emulator.executeCycle();
+
+		if (SDL_PollEvent(&event)) {
+			switch (event.type)  {
+
+				case SDL_KEYDOWN:
+					emulator.pressKey(event.key.keysym.sym);
+					break;
+
+				case SDL_KEYUP:
+					emulator.unpressKey(event.key.keysym.sym);
+					break;
+			}
+		}
+
+		if (emulator.drawFlag) {
+			drawGraphics(ren, texture, renderBuff, emulator.gfxBuffer());
+			//emulator.gfxDraw(std::cout);
+		}
+		//std::cin.get();
+	}
 
 	SDL_Quit();
 
 
 	return 0;
 }
+
 /*
 void oldGameLoop(int argc, char* argv[]) {
 	if (argc != 2)
